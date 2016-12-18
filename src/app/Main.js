4a7af3f21efc4a4905be454464cf93b62afd6898
dqisme <dqisme@outlook.com>
2016-12-18 18:51:02 +0800

    [dq] #N/A basic component and styles

--- src/app/Main.js
+++ src/app/Main.js
@@ -2,10 +2,11 @@
  * In this file, we create a React component
  * which incorporates components provided by Material-UI.
  */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
+import { deepOrange500 } from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -36,45 +37,42 @@ class Main extends Component {
     this.setState({
       open: false,
     });
-  }
+  };
 
   handleTouchTap = () => {
     this.setState({
       open: true,
     });
-  }
+  };
 
-  render() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
+  getStandardActions = () => (
+    <FlatButton
+      label="Ok"
+      primary={true}
+      onTouchTap={this.handleRequestClose}
+    />
+  );
 
-    return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+  render = () => (
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <div style={styles.container}>
+        <Dialog
+          open={this.state.open}
+          title="What do you want to bb"
+          actions={this.getStandardActions()}
+          onRequestClose={this.handleRequestClose}
+        >
+          <TextField fullWidth hintText="bb something..." />
+        </Dialog>
+        <h1>Just BB.</h1>
+        <RaisedButton
+          label="BB"
+          secondary={true}
+          onTouchTap={this.handleTouchTap}
+        />
+      </div>
+    </MuiThemeProvider>
+  );
 }
 
 export default Main;
